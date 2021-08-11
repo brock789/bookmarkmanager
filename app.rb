c03576355ef980e0f2464a0c47fb49430ee3c048
brock789 <brockway789@googlemail.com>
2021-08-11 20:09:37 +0100

    recreating viewing bookmarks

---
+++ app.rb
@@ -0,0 +1,19 @@
+# in app.rb
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+require './lib/bookmark'
+
+class BookMarkManager < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
+  end
+  
+  
+  run! if app_file == $0
+end
